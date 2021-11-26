ff9a142dd5fe81fed0bdbe4ff8a7078d9809ea5b
Chihcheng Hsieh <37566901+ChihchengHsieh@users.noreply.github.com>
2021-11-26 18:33:29 +1000

    introduce patient level.

--- data_path.py
+++ data_path.py
@@ -1,7 +1,7 @@
 import os
 
-# disk_location = "E:/AI-VR dataset"
-disk_location = "/Volumes/New Volume/AI-VR dataset"
+disk_location = "E:/AI-VR dataset"
+# disk_location = "/Volumes/New Volume/AI-VR dataset"
 
 # Define the path for all of the folders.
 ED_FOLDER_PATH = os.path.join(disk_location, "MIMIC-IV ED")
@@ -15,6 +15,5 @@ EYEGAZE_FOLDER_PATH = os.path.join(
     disk_location, "eye-gaze-data-for-chest-x-rays-1.0.0")
 REFLACX_FOLDER_PATH = os.path.join(disk_location, "reflacx-reports-and-eye-tracking-data-for-localization-of-abnormalities-in-chest-x-rays-1.0.0",
                                    "reflacx-reports-and-eye-tracking-data-for-localization-of-abnormalities-in-chest-x-rays-1.0.0")
-# XAMI_MIMIC_PATH = "{disk_location}/XAMI-MIMIC"
-XAMI_MIMIC_PATH = "./XAMI-MIMIC"
+XAMI_MIMIC_PATH = os.path.join(disk_location, "XAMI-MIMIC")
 XAMI_SPREADSHEET_FOLDER_PATH = os.path.join(XAMI_MIMIC_PATH, "speadsheets")
