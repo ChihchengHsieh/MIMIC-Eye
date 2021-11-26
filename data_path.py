dc53c88bf8c50a5289fb1f919701c1fe20e395d8
Chihcheng Hsieh <37566901+ChihchengHsieh@users.noreply.github.com>
2021-11-26 18:34:04 +1000

    run testing.

--- data_path.py
+++ data_path.py
@@ -1,7 +1,10 @@
 import os
 
 # disk_location = "E:/AI-VR dataset"
-disk_location = "/Volumes/New Volume/AI-VR dataset"
+# disk_location = "/Volumes/New Volume/AI-VR dataset"
+disk_location = "/Volumes/Seagate Backup "
+
+
 
 # Define the path for all of the folders.
 ED_FOLDER_PATH = os.path.join(disk_location, "MIMIC-IV ED")
@@ -13,8 +16,7 @@ CXR_DICOM_FOLDER_PATH = os.path.join(
     disk_location, "MIMIC-CXR", "physionet.org", "files", "mimic-cxr", "2.0.0")
 EYEGAZE_FOLDER_PATH = os.path.join(
     disk_location, "eye-gaze-data-for-chest-x-rays-1.0.0")
-REFLACX_FOLDER_PATH = os.path.join(disk_location, "reflacx-reports-and-eye-tracking-data-for-localization-of-abnormalities-in-chest-x-rays-1.0.0",
-                                   "reflacx-reports-and-eye-tracking-data-for-localization-of-abnormalities-in-chest-x-rays-1.0.0")
-# XAMI_MIMIC_PATH = "{disk_location}/XAMI-MIMIC"
-XAMI_MIMIC_PATH = "./XAMI-MIMIC"
+REFLACX_FOLDER_PATH = os.path.join(disk_location, "reflacx-reports-and-eye-tracking-data-for-localization-of-abnormalities-in-chest-x-rays-1.0.0")
+XAMI_MIMIC_PATH = os.path.join(disk_location, "XAMI-MIMIC")
+# XAMI_MIMIC_PATH = "./XAMI-MIMIC"
 XAMI_SPREADSHEET_FOLDER_PATH = os.path.join(XAMI_MIMIC_PATH, "speadsheets")
